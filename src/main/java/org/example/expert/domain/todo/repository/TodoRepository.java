97b6ac192701cc8c0ceaa5f607634c1345282f74
dawn0920 <saoul0920@gmail.com>
2025-05-07 17:09:06 +0900

    Lv1 - 3. 코드 개선 퀴즈 -  JPA의 이해

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
@@ -18,4 +20,24 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             "LEFT JOIN t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
+    @Query(value = "SELECT t FROM Todo t LEFT JOIN FETCH t.user u " +
+            "WHERE (:weather IS NULL OR t.weather = :weather) " +
+            "AND (:start IS NULL OR t.modifiedAt >= :start) " +
+            "AND (:end IS NULL OR t.modifiedAt <= :end) " +
+            "ORDER BY t.modifiedAt DESC",
+            countQuery = "SELECT COUNT(t) FROM Todo t " +
+                    "WHERE (:weather IS NULL OR t.weather = :weather) " +
+                    "AND (:start IS NULL OR t.modifiedAt >= :start) " +
+                    "AND (:end IS NULL OR t.modifiedAt <= :end)")
+    Page<Todo> searchTodos(
+            @Param("weather") String weather,
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end,
+            Pageable pageable);
+    // LEFT JOIN FETCH t.user u - 유저 정보까지 한 번에 가지고옴
+    // :weather IS NULL OR t.weather = :weather - 시작일이 없으면 조건 무시
+    // :weather -> 메서드에서 넘겨주는 파라미터 값
+    //  countQuery = "SELECT COUNT(t) FROM Todo t WHERE ..."
+    // - Page<Todo>로 받을 때 JPA는 본문 쿼리로 데이터를 가져오가 + countQuery 전체 데이터 개수 세기 두가지 작업을 함
 }
